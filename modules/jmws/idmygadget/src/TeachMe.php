ec7af4324d3746263d5116f034a9911163934e9c
Tom W Hartung <tomwhartung@gmail.com>
2015-12-24 20:02:26 -0700

    The namespace declarations, use statements, and etc. are not working the way I am thinking they should, so I will learn by experimenting, starting with a simple example and building up to what I want from there.

---
+++ modules/jmws/idmygadget/src/TeachMe.php
@@ -0,0 +1,13 @@
+<?php
+namespace Drupal\idMyGadget;
+
+class TeachMe {
+	public function __construct() {
+		// print 'Teach me.';
+		error_log( 'Teach me.' );
+	}
+
+	public function teachMe() {
+		error_log( 'Teach me.' );
+	}
+}
