a577656fed7c25998b578a7fe4e29ff28a47a488
Tom W Hartung <tomwhartung@gmail.com>
2015-12-24 20:21:20 -0700

    Adding directory, file, and class named LearningMore , which is structured closer to what we want to do with the IdMyGadget subdirectory.

---
+++ modules/jmws/idmygadget/src/LearningMore/LearningMore.php
@@ -0,0 +1,13 @@
+<?php
+namespace Drupal\idMyGadget\LearningMore;
+
+class LearningMore {
+	public function __construct() {
+		// print 'LearningMore today.';
+		error_log( 'LearningMore today.' );
+	}
+
+	public function learningMore() {
+		error_log( 'LearningMore today.' );
+	}
+}
