d13d31c0abb08ea9f4c4bfdaff2306ee93fb0144
Tom W Hartung <tomwhartung@gmail.com>
2015-12-27 11:10:07 -0700

    Updated comments and added code to get the freshly-created jmwsIdMyGadget object.  Added an error_log statement to prove we have the object.

--- modules/jmws/idmygadget/src/JmwsIdMyGadget/GetJmwsIdMyGadgetDrupal.php
+++ modules/jmws/idmygadget/src/JmwsIdMyGadget/GetJmwsIdMyGadgetDrupal.php
@@ -1,6 +1,12 @@
 <?php
 namespace Drupal\idmygadget\JmwsIdMyGadget {
 
+  /** GetJmwsIdMyGadgetDrupal is analogous to GoGlobal/NamespacedService.php in
+   *  the jmws_drupal_hello_world-my_hugs-D8 repo.
+   * This class is key in that it creates a jmwsIdMyGadget global object,
+   *  like the one we use in WP and Joomla, and makes it available via the Drupal service class.
+   *
+   */
   class GetJmwsIdMyGadgetDrupal {
     public function __construct( $createdBy='' ) {
       error_log( $createdBy . ' created this GetJmwsIdMyGadgetDrupal object.' );
@@ -19,9 +25,13 @@ namespace Drupal\idmygadget\JmwsIdMyGadget {
 }
 //
 // This code is in the global namespace
+// This is where we create the jmwsIdMyGadget object, which is global (as it is in WP and Joomla)
 //
 namespace {
   global $jmwsIdMyGadget;
   require_once 'JmwsIdMyGadgetDrupal.php';
   $jmwsIdMyGadget = new JmwsIdMyGadgetDrupal( 'tera_wurfl' );
+  $supportedGadgetDetectors = $jmwsIdMyGadget->getSupportedGadgetDetectors();
+  error_log( 'Global namespace in GetJmwsIdMyGadgetDrupal.php: $supportedGadgetDetectors[0] = ' . $supportedGadgetDetectors[0] );
+
 }
