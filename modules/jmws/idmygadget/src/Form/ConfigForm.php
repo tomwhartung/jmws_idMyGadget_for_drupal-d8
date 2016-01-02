2abb6e9b241e744c816bfb259d57d4d48e933059
Tom W Hartung <tomwhartung@gmail.com>
2016-01-01 20:11:43 -0700

    Added function siteNameOptions that should work but is not.  Baselining this version before experimenting more with this and eventually fixing it.

--- modules/jmws/idmygadget/src/Form/ConfigForm.php
+++ modules/jmws/idmygadget/src/Form/ConfigForm.php
@@ -11,6 +11,7 @@ use Drupal\Core\Form\FormStateInterface;
 // use Drupal\idMyGadget\JmwsIdMyGadget;
 
 class ConfigForm extends ConfigFormBase {
+
   /**
    * {@inheritdoc}
    */
@@ -38,17 +39,13 @@ class ConfigForm extends ConfigFormBase {
       '#type' => 'radios',
       '#title' => t('Gadget Detector (ConfigForm)'),
       // '#default_value' => $supported_gadget_detectors[0],
-	  '#default_value' => $config->get('idmygadget_gadget_detector'),
+      '#default_value' => $config->get('idmygadget_gadget_detector'),
       '#options' => $supported_gadget_detectors,
       '#description' => $this->t('Select the 3rd party device detector to use for this site.'),
       '#required' => TRUE,
    );
-
-    $form['idmygadget_'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Experimental textfield input defined in ConfigForm class'),
-      '#default_value' => $config->get('exp_textfield'),
-    ];
+   // $formSiteNamePhones = $this->siteNameOptions( 'phone' );
+   // $form = array_merge( $form, $formSiteNamePhones );
 
     return parent::buildForm($form, $form_state);
   }
@@ -68,4 +65,43 @@ class ConfigForm extends ConfigFormBase {
   public function getEditableConfigNames() {
     return ['idmygadget.settings'];
   }
+
+  protected function siteNameOptions( $gadgetType='desktop' ) {
+/*
+    $form['idmygadget_'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Experimental textfield input defined in ConfigForm class'),
+      '#default_value' => $config->get('exp_textfield'),
+    ];
+ */
+    $gadgetTypePlural = $gadgetType . 's';
+    $gadgetTypeUcfirst = ucfirst( $gadgetType );
+    $gadgetTypePluralUcfirst = ucfirst( $gadgetTypePlural );
+
+    $config = $this->config('idmygadget.settings');
+
+    $radioChoices = array( 'No', 'Yes' );  // NOTE: 'No' must be the zeroeth elt ;-)
+    $settingName = 'idmygadget_show_site_name_' . $gadgetType;   // e.g., 'idmygadget_show_site_name_phone'
+    $form[$settingName] = array(
+      '#type' => 'radios',
+      '#title' => t( 'Show Site Name on ' . $gadgetTypePluralUcfirst . '?' ),
+      '#default_value' => $config->get( $settingName ),
+      '#options' => $radioChoices,
+      '#description' => t( 'Select whether you want the name of this site to display in the header on ' . $gadgetTypePlural . '.' ),
+      '#required' => TRUE,
+    );
+
+    $validElements = array( 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'div', 'span' );
+    $settingName = 'idmygadget_site_name_element_' . $gadgetType;     // e.g., 'idmygadget_site_name_element_phone'
+    $form[$settingName] = array(
+      '#type' => 'select',
+      '#title' => t( 'Site Name Element ' . $gadgetTypeUcfirst ),
+      '#default_value' => $config->get( $settingName ),
+      '#options' => $validElements,
+      '#description' => t( 'Select the html element in which you want to display the name of this site in the header on ' . $gadgetTypePlural . '.' ),
+      '#required' => FALSE,
+    );
+  }
+
+
 }
