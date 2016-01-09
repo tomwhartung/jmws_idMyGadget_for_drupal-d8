0ec0d63ac9ed3a85065038d66c2dfc95d1df0ec9
Tom W Hartung <tomwhartung@gmail.com>
2016-01-08 19:32:37 -0700

    Cleaned up a few things and added function header to each method in src/Plugin/Block/IdMyGadgetStatus.php .

--- modules/jmws/idmygadget/src/Plugin/Block/IdMyGadgetStatus.php
+++ modules/jmws/idmygadget/src/Plugin/Block/IdMyGadgetStatus.php
@@ -1,17 +1,17 @@
 <?php
-
 namespace Drupal\idmygadget\Plugin\Block;
-
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\idmygadget\IdMyGadgetService;
-use Drupal\idmygadget\JmwsIdMyGadget;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Reports on idmygadget status.
+ * This code is heavily inspired by Larry Garfield's Hugs module, presented at the
+ * Drupal Con in LA in 2015.
+ * Also see the my_hugs module in the jmws_drupal_hello_world-my_hugs-d8 repo
  *
  * @Block(
  *   id = "idmygadget_status",
@@ -33,12 +33,29 @@ class IdMyGadgetStatus extends BlockBase implements ContainerFactoryPluginInterf
    */
   protected $jmwsIdMyGadget = null;
 
+  /**
+   * Creates an instance of this class, should be called via create()
+   *
+   * @param array $configuration
+   * @param type $plugin_id
+   * @param type $plugin_definition
+   * @param IdMyGadgetService $idMyGadgetService
+   */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, IdMyGadgetService $idMyGadgetService) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->idMyGadgetService = $idMyGadgetService;
     $this->jmwsIdMyGadget = $this->idMyGadgetService->jmwsIdMyGadget;
   }
 
+  /**
+   * Returns an instance of this class, calling the constructor with appropriate arguments
+   *
+   * @param ContainerInterface $container
+   * @param array $configuration
+   * @param type $plugin_id
+   * @param type $plugin_definition
+   * @return \static
+   */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
       $configuration, $plugin_id, $plugin_definition,
@@ -46,10 +63,22 @@ class IdMyGadgetStatus extends BlockBase implements ContainerFactoryPluginInterf
     );
   }
 
+  /**
+   * Default configuration for enabling module (via Extend screen in back end)
+   *
+   * @return type
+   */
   public function defaultConfiguration() {
     return ['enabled' => 1];
   }
 
+  /**
+   * Set the form for enabling module (via Extend screen in back end)
+   *
+   * @param type $form
+   * @param FormStateInterface $form_state
+   * @return type
+   */
   public function blockForm($form, FormStateInterface $form_state) {
     $form['enabled'] = [
       '#type' => 'checkbox',
@@ -60,6 +89,12 @@ class IdMyGadgetStatus extends BlockBase implements ContainerFactoryPluginInterf
     return $form;
   }
 
+  /**
+   * Submit the form for enabling module (via Extend screen in back end)
+   *
+   * @param type $form
+   * @param FormStateInterface $form_state
+   */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['enabled'] = (bool)$form_state->getValue('enabled');
   }
