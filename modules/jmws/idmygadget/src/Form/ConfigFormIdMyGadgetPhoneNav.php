bfcf1f77afa2262a9a03519185fa630b33fc07ca
Tom W Hartung <tomwhartung@gmail.com>
2016-01-28 01:41:15 -0700

    Updated ConfigFormIdMyGadgetPhoneNav.php to display the phone nav options.

--- modules/jmws/idmygadget/src/Form/ConfigFormIdMyGadgetPhoneNav.php
+++ modules/jmws/idmygadget/src/Form/ConfigFormIdMyGadgetPhoneNav.php
@@ -5,8 +5,6 @@
  * Reference:https://www.drupal.org/node/2206551
  */
 namespace Drupal\idmygadget\Form;
-
-use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 // use Drupal\idMyGadget\JmwsIdMyGadget;
 
@@ -16,28 +14,16 @@ class ConfigFormIdMyGadgetPhoneNav extends ConfigFormIdMyGadgetBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'idmygadget_config_detector';
+    return 'idmygadget_config_phone_nav';
   }
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('idmygadget.settings');
-
-    //
-    // Add a section to the module's Settings screen that contains
-    // radio buttons allowing the admin to set the device detector.
-    // This shows up under Configuration -> IdMyGadget -> Gadget Detector
-    //
-    $form['idmygadget_gadget_detector'] = array(
-      '#type' => 'radios',
-      '#title' => t('Gadget Detector'),
-      // '#default_value' => $this->supportedGadgetDetectors[0],
-      '#default_value' => $config->get('idmygadget_gadget_detector'),
-      '#options' => $this->supportedGadgetDetectors,
-      '#description' => $this->t('Select the 3rd party device detector to use for this site.'),
-      '#required' => TRUE,
-    );
+    foreach( $this->gadgetTypes as $gadgetType ) {
+      $formPhoneNavOptions = $this->phoneNavOptions( $gadgetType );
+      $form = array_merge( $form, $formPhoneNavOptions );
+    }
 
     return parent::buildForm($form, $form_state);
   }
@@ -46,9 +32,15 @@ class ConfigFormIdMyGadgetPhoneNav extends ConfigFormIdMyGadgetBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-
     $config = $this->config('idmygadget.settings');
-    $config->set('idmygadget_gadget_detector', $form_state->getValue('idmygadget_gadget_detector'));
+
+    // $config->set('idmygadget_jqm_data_theme', $form_state->getValue('idmygadget_jqm_data_theme'));
+
+    foreach( $this->gadgetTypes as $gadgetType ) {
+      $gadgetTypePlural = $gadgetType . 's';
+      $settingName = 'idmygadget_phone_nav_on_' . $gadgetTypePlural;  // e.g., 'idmygadget_phone_nav_on_phones'
+      $config->set( $settingName, $form_state->getValue($settingName) );
+    }
 
     $config->save();
   }
