6c849bf0ec4bf75101a315af7d51a8bf81e446b0
Tom W Hartung <tomwhartung@gmail.com>
2016-02-04 19:34:16 -0700

    Commented out call to error_log in the constructor and the global function and added some comments about how it works and why I did it that way.

--- modules/jmws/idmygadget/src/JmwsIdMyGadget/GetJmwsIdMyGadgetDrupal.php
+++ modules/jmws/idmygadget/src/JmwsIdMyGadget/GetJmwsIdMyGadgetDrupal.php
@@ -1,8 +1,17 @@
 <?php
 namespace Drupal\idmygadget\JmwsIdMyGadget {
 
-  /** GetJmwsIdMyGadgetDrupal is analogous to GoGlobal/NamespacedService.php in
-   *  the jmws_drupal_hello_world-my_hugs-D8 repo.
+  /**
+   * GetJmwsIdMyGadgetDrupal is analogous to GoGlobal/NamespacedService.php in
+   *   the jmws_drupal_hello_world-my_hugs-D8 repo.
+   * If you think this is a hack you may want to check out that repo before judging,
+   *   it is evidence that I tried many things before deciding on this technique.
+   * Admitedly much of this is due to the fact that, for the other CMSes (WP and Joomla)
+   *   it was copacetic (if not a requirement) to use a global object (albeit uniquely-named, duh!)
+   *   to encapsulate the device detection functionality.
+   * Please forgive my whining but this Symfony service and namespace stuff is all rather new
+   *   (and at this time unique to Drupal).  Note to self: do Drupal first next time! ;-)
+   *
    * This class is key in that it creates a jmwsIdMyGadget global object,
    *  like the one we use in WP and Joomla, and makes it available via the Drupal service class.
    *
@@ -22,7 +31,7 @@ namespace Drupal\idmygadget\JmwsIdMyGadget {
      */
     public function __construct( $gadgetDetectorIndex=0 ) {
       $this->gadgetDetectorIndex = $gadgetDetectorIndex;
-      error_log( 'GetJmwsIdMyGadgetDrupal constructor: $gadgetDetectorIndex = "' . $gadgetDetectorIndex . '"' );
+      // error_log( 'GetJmwsIdMyGadgetDrupal constructor: $gadgetDetectorIndex = "' . $gadgetDetectorIndex . '"' );
       $this->getJmwsIdMyGadget();
     }
 
@@ -45,8 +54,13 @@ namespace Drupal\idmygadget\JmwsIdMyGadget {
   }
 }
 //
-// This code is in the global namespace
+// ************************************************
+// ** NOTE: this code is in the global namespace **
+// ************************************************
 // It should be run only once, when the class above is instantiated.
+// Note to self:
+//   Next time we decide to do the same project in these three CMSes, do the Drupal version first,
+//      because the resultant code will be better organized from the get-go!
 //
 namespace {
   require_once 'JmwsIdMyGadgetDrupal.php';
@@ -70,7 +84,7 @@ namespace {
     if( $jmwsIdMyGadget == null ) {
       $gadgetDetectorString = JmwsIdMyGadgetDrupal::$supportedGadgetDetectors[$gadgetDetectorIndex];
       $jmwsIdMyGadget = new JmwsIdMyGadgetDrupal( $gadgetDetectorString );
-      error_log( 'global fcn createSetAndGetJmwsIdMyGadget: $gadgetDetectorString "' . $gadgetDetectorString . '"' );
+      // error_log( 'global fcn createSetAndGetJmwsIdMyGadget: $gadgetDetectorString "' . $gadgetDetectorString . '"' );
     }
 
     return $jmwsIdMyGadget;
